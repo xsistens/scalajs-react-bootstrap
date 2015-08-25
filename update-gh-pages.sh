6a9864c9fce66f6c419ed39e9f2426b491c48cd1
Christian <chriz.kirchner@googlemail.com>
2015-08-25 21:41:12 +0200

    Fix gp-pages

---
+++ update-gh-pages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+
+ii=gh-pages/index.html
+io=index.html
+js=gh-pages/res/ghpages.js
+
+rm -f $io $js
+
+cat $ii | perl -pe '
+    s!"res/!"gh-pages/res/!g;
+    s!"target/.+?\.js"!"'"$js"'"!g;
+    ' > $io \
+  && sbt gh-pages/fullOptJS \
+  && git add $js $io \
+  && git status \
+  && echo "git commit -m 'Refresh gh-pages'" && echo
